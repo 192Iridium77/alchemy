6fc9aa2c2077ad969cc91e2d07c308936c6fd68b
Matt Martin <matt.martin@sine.co>
2022-07-19 17:01:07 +0930

    update tech drive blog

--- src/Articles/data.js
+++ src/Articles/data.js
@@ -36,7 +36,7 @@ const articles = [
     ],
     author: "Matthew Martin",
     draft: true,
-    publishedDate: new Date(),
+    publishedDate: new Date("07/01/2022 18:00"),
   }),
   new Article({
     title: "Must Reads - Business",
@@ -51,13 +51,13 @@ const articles = [
     components: [],
     author: "Matthew Martin",
     draft: true,
-    publishedDate: new Date(),
+    publishedDate: new Date("05/12/2022 13:00"),
   }),
   new Article({
     title: "Tech Drive",
     slug: "tech-drive",
     description:
-      "<p>How did you feel when you first started programming? People I have asked have shared their experience as awe at the potential that opens up and dopamine rush of excitement to discover more.</p>",
+      "<p>Do you remember what it was like when you first discovered programming? People I have asked have shared their experience as awe at the potential that opens up coupled with desire to discover more.</p>",
     image: {
       filename: "cityFeature",
       extension: "jpg",
@@ -68,29 +68,30 @@ const articles = [
         type: "markdown",
         data: {
           html: `
-          <p>I found the possibilities exhilarating, I was playing around with Arduinos and a Raspberry Pi 3B and I the whole world opened up. I wanted to take control of everything around me, to make the heater turn on in the morning, to make a farm that automatically grew produce and to build solar panels to power my microcontrollers and light up my room and power the fan in the summer.</p>
-  
-  <p>I wanted to do this every day so I spruced up my resume and applied for developer jobs and was eventually hired at a start up agency called Svelte Studios. On the back of the fumes of this passion I often stayed in the office and would often get home at 11pm at night. Not once though did I really think carefully about what I was doing. I think it is often the case for programmers, without much of a real plan we follow the scent of excitement wherever it takes us and rarely stop to think about what we are aiming for.</p>
-  
-  <p>Last month I discovered a book called The Passionate Programmer by Chad Fowler. In this book Chad asks us to look at are careers like investments into a company. You would never invest your money in a company without really researching first. You might even consider re-evaluating your investment every quarter or so depending on the kind of investment you are making. Inspired by this, I gave my own career pathway some love and care, and in doing so I can aim for higher and harder goals. This act of aiming higher as the Author of The Art of Impossible suggests when implemented correctly enhances your motivation by 11-25%.</p>
-  
-  <p>Finding where to aim your career can be challenging however. What if nobody will pay you to do what you love to do? What if you hate doing what people would pay you big money for? Perhaps you aren&rsquo;t aware what pathways are available. What we really want is to find the cross section where we are maximally interested in a field or topic, while still catering to the market. To achieve this, I decided I would set out with a simple spread sheet like so:</p>
+            <p>When I first started to code, the endless potential was intoxicating. I learned python, bash and C while playing around with Arduinos and a Raspberry Pi 3B and wanted to learn everything. I wanted to control the appliances around me, to make the heater turn on in the morning, to make a farm that automatically grows produce and to build solar panels to power my microcontrollers and light up my room and power the fan in the summer.</p>
+            
+            <p>I wished to do this every day so I spiced up my resume and applied for developer jobs and was eventually hired at a start up agency. On the back of the fumes of this passion I often stayed in the office and would come home at 11pm at night. Not once though did I really think carefully about what I was doing. I think it is often the case for programmers that they are living on this excitement, without much of a real plan, we may not stop to think about what we are aiming to achieve.</p>
+            
+            <p>Last month I discovered a book called The Passionate Programmer by Chad Fowler. In this book Chad asks us to look at are careers like investments into a company. You would never invest your money in a company without really researching first! You might even consider re-evaluating your investment every quarter or so. Inspired by this, I gave my own career pathway some love and care, and in doing so I aim for higher and harder goals in my work. This act of aiming higher as the Author of The Art of Impossible suggests when implemented correctly enhances your motivation by 11-25%.</p>
+            
+            <p>Finding where to aim your career can be a real challenge however. What if nobody will pay you to do what you love to do? What if you hate doing what people would pay you big money to do? Perhaps you don't know what pathways are available. What we really want is to find the cross section where we are maximally interested in a field or topic, while still catering to the market. To achieve this, I decided to apply data and metrics like so:</p>
 
-  * Table here *
+            * Table here *
 
-  <p>Your list could be very different from this one depending on your options, awareness and interests. I found career options for software development by searching online and using similar career suggestions on common salary websites. If there are some missing, it&rsquo;s either because I have no interest in that area or am not aware of it yet. The interest level I determined using a scale suggested by the book &ldquo;The art of the Impossible&rdquo;, you can gauge your interest based on whether you would spend an entire weekend studying or working on this topic, getting in touch with specialists or reading books on the topic. The market value I determined using average salaries for Australia, this could look very different for you based on just location, but you could also take into account the number of job postings available for the job.</p>
-  
-  <p>The key to this table is the Interest * Value column. Here I have presumed that having an interesting job and having a valuable job are equally weighted, and having both together multiplies the value because the two have highest synergy together. I term this the Career Investment Score, and after having sorted my career options by this value, I could more easily determine an optimal pathway forwards. Before making this chart, I was considering Embedded software and Web app development to be my best options, now I have a much broader picture.</p>
-  
-  <p>One of the challenges that this plot hasn&rsquo;t overcome is the difficulty and investment requirements for particular career options. The glaring example being that software architecture requires years of experience in general software development, but regardless it has given me a much needed high goal to aim for. I figure making an app that combines this chart with a skills tree could help give people who are lost in their careers a highly personalised map forward.</p>
-  
-  <p>In this article, I have set out to take control of my career and to set higher goals to achieve a career I can be happy in and proud of, and using this table I feel I have carved a new way forward. I hope that I can make this easy for other people to try it too.</p>`,
+            <p>Your list could be very different from this one depending on your options and interests. I found career options for software development by searching online and using similar career suggestions on common salary websites. If there are some missing, it&rsquo;s either because I have no interest in that area or am not aware of it yet. The interest level I determined using a scale suggested by the book &ldquo;The Art of the Impossible&rdquo;, you can gauge your interest based on whether you would spend an entire weekend studying or working on this topic, getting in touch with specialists or reading books on the topic. The market value I determined using average salaries for Australia, this could look very different for you based on just location, but you could also take into account the number of job postings available for the job.</p>
+            
+            <p>The key to this table is the Interest * Value column. Here I have presumed that having an interesting job and having a valuable job are equally weighted, and having both together multiplies the value because the two have highest synergy together. I term this the Career Investment Score. After sorting my career options by this value, I could more easily determine an optimal pathway forwards. Before making this chart, I was considering Embedded software and Web app development to be my best options, now I have a much broader picture.</p>
+            
+            <p>One of the challenges that this plot hasn&rsquo;t overcome is the difficulty and investment requirements for particular career options. The glaring example being that software architecture requires years of experience in general software development, but regardless it has given me a much needed higher goal to aim for. I figure making an app that combines this chart with a skills tree could help give people who are lost in their careers a highly personalised map forward.</p>
+            
+            <p>In this article, I have set out to take control of my career and to set higher goals to achieve a career I can be happy in and proud of, and using this table I feel I have carved a new way forward. I hope that I can make this easy for other people to try it too.</p>
+          `,
         },
       },
     ],
     author: "Matthew Martin",
     draft: true,
-    publishedDate: new Date(),
+    publishedDate: new Date("08/07/2021 15:00"),
   }),
   new Article({
     title: "Code confidence with Typescript",
@@ -122,7 +123,7 @@ const articles = [
     ],
     author: "Matthew Martin",
     draft: true,
-    publishedDate: new Date("07/01/2021 09:00"),
+    publishedDate: new Date("07/03/2021 09:00"),
   }),
 ];
 
