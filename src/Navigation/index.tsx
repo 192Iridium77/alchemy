c76dfd871d2774961bf31da77b3d9c9abb957a32
Matt Martin <matt.martin0108@gmail.com>
2023-03-28 17:25:49 +1030

    container for top nav, split into mobile and desktop nav

--- src/Navigation/index.tsx
+++ src/Navigation/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { TopBar } from "alchemy-tech-ui";
+import { TopBar, MobileTopBar } from "alchemy-tech-ui";
 import { useMediaQuery } from "react-responsive";
 import Modal from "react-modal";
 import SignUp from "../User/SignUp";
@@ -23,13 +23,18 @@ function Navigation() {
   const [modalIsOpen, setIsOpen] = React.useState(false);
 
   let maxWidth;
-  const isDesktopSmall = useMediaQuery({ query: "(min-width: 1024px)" });
-  const isDesktopMedium = useMediaQuery({ query: "(min-width: 1280px)" });
-  const isDesktopLarge = useMediaQuery({ query: "(min-width: 1536px)" });
+  // todo make some kind of responsive hook
+  const isLargeDesktop = useMediaQuery({ query: "(min-width: 1536px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width: 1280px)" });
+  const isSmallDesktop = useMediaQuery({ query: "(min-width: 1024px)" });
 
-  if (isDesktopSmall) maxWidth = "1024px";
-  if (isDesktopMedium) maxWidth = "1280px";
-  if (isDesktopLarge) maxWidth = "1536px";
+  if (isLargeDesktop) {
+    maxWidth = "1536px";
+  } else if (isDesktop) {
+    maxWidth = "1280px";
+  } else if (isSmallDesktop) {
+    maxWidth = "1024px";
+  }
 
   const openSignUp = () => {
     setIsOpen(true);
@@ -41,45 +46,87 @@ function Navigation() {
 
   return (
     <>
-      <TopBar
-        logo={
-          <div className="flex h-full items-center ">
-            <a href="/">
-              <img src="/AlchemyTechLogo.png" className="h-10" alt="logo" />
-            </a>
-          </div>
-        }
-        navigationMenu={
-          <div className="flex flex-col gap-8 bg-white drop-shadow-lg p-4">
-            <Link to="/" className="border-b border-primary-200">
-              Articles
-            </Link>
-            <Link to="/apps" className="border-b border-primary-200">
-              Apps
-            </Link>
-            <Link to="/about" className="border-b border-primary-200">
-              About
-            </Link>
-          </div>
-        }
-        userMenu={
-          <div className="flex flex-col gap-8 bg-white drop-shadow-lg p-4">
-            <button
-              onClick={openSignUp}
-              className="text-primary-200 border-b border-primary-200"
-            >
-              Sign Up
-            </button>
-            {/* <button className="text-primary-200 border-b border-primary-200">
+      {isSmallDesktop ? (
+        <TopBar
+          logo={
+            <div className="flex h-full items-center pl-4">
+              <a href="/">
+                <img src="/AlchemyTechLogo.png" className="h-10" alt="logo" />
+              </a>
+            </div>
+          }
+          navigationMenu={
+            <div className="flex justify-center gap-8 p-4">
+              <Link to="/" className="border-b border-primary-200">
+                Articles
+              </Link>
+              <Link to="/apps" className="border-b border-primary-200">
+                Apps
+              </Link>
+              <Link to="/about" className="border-b border-primary-200">
+                About
+              </Link>
+            </div>
+          }
+          userMenu={
+            <div className="flex gap-8 p-4">
+              <button
+                onClick={openSignUp}
+                className="text-primary-200 border-b border-primary-200"
+              >
+                Sign Up
+              </button>
+              {/* <button className="text-primary-200 border-b border-primary-200">
+          Log In
+        </button> */}
+              {/* <button className="text-primary-200 border-b border-primary-200">
+          Log Out
+        </button> */}
+            </div>
+          }
+          maxWidth={maxWidth}
+        />
+      ) : (
+        <MobileTopBar
+          logo={
+            <div className="flex h-full items-center ">
+              <a href="/">
+                <img src="/AlchemyTechLogo.png" className="h-10" alt="logo" />
+              </a>
+            </div>
+          }
+          navigationMenu={
+            <div className="flex flex-col gap-8 bg-white drop-shadow-lg p-4">
+              <Link to="/" className="border-b border-primary-200">
+                Articles
+              </Link>
+              <Link to="/apps" className="border-b border-primary-200">
+                Apps
+              </Link>
+              <Link to="/about" className="border-b border-primary-200">
+                About
+              </Link>
+            </div>
+          }
+          userMenu={
+            <div className="flex flex-col gap-8 bg-white drop-shadow-lg p-4">
+              <button
+                onClick={openSignUp}
+                className="text-primary-200 border-b border-primary-200"
+              >
+                Sign Up
+              </button>
+              {/* <button className="text-primary-200 border-b border-primary-200">
             Log In
           </button> */}
-            {/* <button className="text-primary-200 border-b border-primary-200">
+              {/* <button className="text-primary-200 border-b border-primary-200">
             Log Out
           </button> */}
-          </div>
-        }
-        maxWidth={maxWidth}
-      />
+            </div>
+          }
+        />
+      )}
+
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
