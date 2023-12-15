fadeabe05e4e292905327998826fa88bb78afebc
Matt Martin <matt.martin0108@gmail.com>
2023-12-15 12:00:30 +1030

    webpack certificate, display certs fix

--- src/about/Certifications.tsx
+++ src/about/Certifications.tsx
@@ -5,22 +5,24 @@ const certificatesData = [
   "go-the-complete-developers-guide",
   "design-patterns-in-javascript",
   "creating-accessible-websites",
+  "webpack-5-in-2023-optimizing-for-production",
 ];
 
 const Certificates = () => {
   return (
     <div>
       <div className="text-xl flex justify-center mt-8">Certificates</div>
-      <div className="flex flex-wrap justify-center mt-8 gap-8 -mx-4">
+      <div className="flex flex-wrap mt-8">
         {certificatesData.map((certificate, index) => (
           <a
             key={index}
-            className="md:w-5/12 p-2 block shadow-md"
+            className="md:w-6/12 p-4 block"
             href={`/certificates/${certificate}.pdf`}
           >
             <img
               src={`/certificates/${certificate}.jpeg`}
               alt={`Certificate ${index + 1}`}
+              className="shadow-md p-2"
             />
           </a>
         ))}
