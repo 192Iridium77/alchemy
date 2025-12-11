1fe2b89b5c5b5a3212b4195d338a238125e7de05
Matt Martin <matt.martin0108@gmail.com>
2025-12-12 10:20:36 +1030

    update seo and og image, delete old images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,25 @@ export const metadata: Metadata = {
   title: "Matt Martin",
   description:
     "Explore my portfolio showcasing projects with global impact. With technical expertise, I bring a blend of creativity and customer focus to every project.",
+  openGraph: {
+    title: "Matt Martin",
+    description:
+      "Explore my portfolio showcasing projects with global impact. With technical expertise, I bring a blend of creativity and customer focus to every project.",
+    images: [
+      {
+        url: "/images/og-image.png", // Update this to your desired image path
+        width: 1200,
+        height: 630,
+        alt: "Matt Martin Portfolio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Matt Martin",
+    description: "Explore my portfolio showcasing projects with global impact.",
+    images: ["/images/og-image.png"], // Update this to your desired image path
+  },
 };
 
 export default function RootLayout({
