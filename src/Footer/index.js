9fc56d8fb0041785789b7e3c11c294759d886899
Matt Martin <matt.martin0108@gmail.com>
2025-12-12 11:14:00 +1030

    align footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,26 +1,10 @@
 import "./style.css";
 
-const EmailIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
-    />
-  </svg>
-);
-
 function Footer() {
   return (
     <div className="bg-white">
       <header className="container flex items-center justify-between py-8">
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center">
           <a className="w-6 h-6 block" href="https://github.com/192Iridium77">
             <img src="/images/github-mark.webp" alt="github" />
           </a>
@@ -33,12 +17,12 @@ function Footer() {
           </a>
           <a
             href="mailto:matt.martin0108@gmail.com"
-            className="text-xs md:text-base"
+            className="text-xs md:text-sm"
           >
             matt.martin0108@gmail.com
           </a>
         </div>
-        <div className="text-xs">Matthew Martin</div>
+        <div className="text-xs md:text-sm">Matthew Martin</div>
       </header>
     </div>
   );
